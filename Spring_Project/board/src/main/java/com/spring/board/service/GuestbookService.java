8078d9cd99b13c5d1f4f47ea1a14b37e2a2c3ca4
minji6313 <minji6313@yahoo.com>
2023-07-27 19:08:22 +0900

    Entity -> Dto

--- Spring_Project/board/src/main/java/com/spring/board/service/GuestbookService.java
+++ Spring_Project/board/src/main/java/com/spring/board/service/GuestbookService.java
@@ -1,17 +1,20 @@
 package com.spring.board.service;
 
 import com.spring.board.dto.GuestbookDTO;
+import com.spring.board.dto.PageRequestDTO;
+import com.spring.board.dto.PageResultDTO;
 import com.spring.board.entity.Guestbook;
 
 //service계층에서는 DTO를 이용해 필요한 내용을 전달받고 반환하도록 처리
 public interface GuestbookService {
 
     Long register(GuestbookDTO dto);
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
 
     /**service에서는 파라미터를 DTO 타입으로 받기 때문에 JPA로 처리하기 위해서는 Entity 타입의 객체로 변환해야 하는 작업이 반드시 필요
      * java 8 버전부터 인터페이스의 실제 내용을 가지는 코드는 default라는 키워드로 생성할 수 있다
      * -> 실제 코드를 인터페이스에 선언할 수 있다 => 추상클래스를 생략하는것이 가능해 졌다*/
-    default Guestbook dtoToEntity(GuestbookDTO dto) {
+     default Guestbook dtoToEntity(GuestbookDTO dto) {
         Guestbook entity = Guestbook.builder()
                 .gno(dto.getGno())
                 .title(dto.getTitle())
@@ -20,5 +23,19 @@ public interface GuestbookService {
                 .build();
 
         return entity;
+    }
+
+    default GuestbookDTO entityToDto(Guestbook entity) {
+
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+
+        return dto;
     }
 }
--- Spring_Project/board/src/main/java/com/spring/board/service/GuestbookServiceImpl.java
+++ Spring_Project/board/src/main/java/com/spring/board/service/GuestbookServiceImpl.java
@@ -2,13 +2,20 @@ package com.spring.board.service;
 
 
 import com.spring.board.dto.GuestbookDTO;
+import com.spring.board.dto.PageRequestDTO;
+import com.spring.board.dto.PageResultDTO;
 import com.spring.board.entity.Guestbook;
 import com.spring.board.repository.GuestbookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor//final 선언 객체 의존성 자동 주입 (Bean)
 public class GuestbookServiceImpl implements GuestbookService {
 
     private final GuestbookRepository repository;
@@ -21,4 +28,16 @@ public class GuestbookServiceImpl implements GuestbookService {
         repository.save(entity);
         return entity.getGno();
     }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno"));
+        Page<Guestbook> result = repository.findAll(pageable);
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+        /** JPA의 처리 결과인 Page<Entity>와 Function을 전달해 엔티티 객체들을 DTO의 리스트로 변환하고
+         * 화면에 페이지 처리와 필요한 값들을 생성 */
+        return new PageResultDTO<>(result,fn);
+    }
+
 }
