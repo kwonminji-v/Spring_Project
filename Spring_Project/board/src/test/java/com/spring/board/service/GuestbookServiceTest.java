766f7808e1a7ee09f4ec6bb8655ebd8317ca3bfd
minji6313 <minji6313@yahoo.com>
2023-08-05 20:12:18 +0900

    "view file 생성"

--- Spring_Project/board/src/test/java/com/spring/board/service/GuestbookServiceTest.java
+++ Spring_Project/board/src/test/java/com/spring/board/service/GuestbookServiceTest.java
@@ -29,10 +29,16 @@ public class GuestbookServiceTest {
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
         PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
 
+        System.out.println("PREV = " + resultDTO.isPrev());
+        System.out.println("Next = " + resultDTO.isNext());
+        System.out.println("Total = " + resultDTO.getTotalPage());
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
         for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
             System.out.println(guestbookDTO);
-
         }
+
+        System.out.println("==========================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 
 }
