cfa4897da22114a2cb0296281ed9627e16ffbff0
minji6313 <minji6313@yahoo.com>
2023-08-06 01:02:52 +0900

    "페이징진행"

--- Spring_Project/board/src/main/java/com/spring/board/controller/GuestBookController.java
+++ Spring_Project/board/src/main/java/com/spring/board/controller/GuestBookController.java
@@ -24,15 +24,15 @@ public class GuestBookController {
     private final GuestbookService service;
 
     @GetMapping("/")
-    public String list() {
+    public String index(){
         return "redirect:/guestbook/list";
     }
 
-    @GetMapping("/list") //실제 보여지는 화면에 페이징 기능을 반영
-    public void list(@ModelAttribute PageRequestDTO pageRequestDTO, Model model) {
-        /** 실제로 model에 추가되는 데이터 :PageResultDTO*/
-        //model을 이용해 GuestBookServiceImple에서 반환하는 PageResultDTO를 result 라는 이름으로 전달
-        log.info("list 목록 ===============" + pageRequestDTO);
+    @GetMapping("/list")
+    public void list(@ModelAttribute PageRequestDTO pageRequestDTO , Model model){
+
+        // 실제로 model에 추가되는 데이터 : PageResultDTO
+        // => Model을 이용해 GuestbookServiecImpl에서 반환하는 PageResultDTO를 result 라는 이름으로 전달
         model.addAttribute("result", service.getList(pageRequestDTO));
     }
 
